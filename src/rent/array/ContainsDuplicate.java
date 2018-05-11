af0473ab822560aa7f30b21755b00af3007243ec
RenT <feveren@163.com>
2018-05-11 18:41:49 +0800

    commit

---
+++ src/rent/array/ContainsDuplicate.java
@@ -0,0 +1,41 @@
+package rent.array;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组，判断是否存在重复元素。
+ *
+ * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ */
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        System.out.println(check(new int[] {1,2,3,1}));
+        System.out.println(check(new int[] {1,2,3,4}));
+        System.out.println(check(new int[] {1,1,1,3,3,4,3,2,4,2}));
+    }
+
+    private static boolean check(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/rent/array/Test.java
@@ -0,0 +1,8 @@
+package rent.array;
+
+public class Test {
+    public static void main(String[] args) {
+        String str1 = "ys7\n117091953\nXEQBOK\nCS-C2C-1A1WFR\n";
+        String str2 = "www.ys7.com\rC13092184\rTVLCNR\r";
+    }
+}
